86bfa98ea821e492d24b687ea02d228bc807f0cd
j00hyun <loove1997@naver.com>
2020-10-06 09:11:32 +0900

    2020-10-07 main storyboard 적용

---
+++ Todobox/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Todobox
+//
+//  Created by Park JooHyun on 2020/10/05.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
